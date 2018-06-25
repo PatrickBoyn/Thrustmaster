cccd53fbfb86589c0dcf84a199c6a5f6aa99c401
Patrick Boynton <dakilangmanok@gmail.com>
2018-06-25 16:00:21 -0400

    Added a script to the rocket. Also added a test behavior to see if it is what I want.

---
+++ Thrustmaster/Assets/Rocket.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        ProcessUpdate();
+	}
+
+    void ProcessUpdate()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            print("Spacebar was pressed!");
+        }
+    }
+}
