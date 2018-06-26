2d1f191c85a02fc37fb50e29e2a7a414a1dbf329
Patrick Boynton <dakilangmanok@gmail.com>
2018-06-25 22:32:05 -0400

    Added audio. Remember to fix the audio artifact.

--- Thrustmaster/Assets/Rocket.cs
+++ Thrustmaster/Assets/Rocket.cs
@@ -1,25 +1,33 @@
-﻿using System;
+﻿using System.Collections;
 using UnityEngine;
 
 public class Rocket : MonoBehaviour {
     Rigidbody rigidbody;
+    AudioSource audioSource; 
 
-
-	// Use this for initialization
+	// Use this for initializationa
 	void Start () {
         rigidbody = GetComponent<Rigidbody>();
-	}
+        audioSource = GetComponent<AudioSource>();
+    }
 	
 	// Update is called once per frame
 	void Update () {
         ProcessUpdate();
 	}
-
     void ProcessUpdate()
     {
         if (Input.GetKey(KeyCode.Space))
         {
             rigidbody.AddRelativeForce(Vector3.up);
+            if (!audioSource.isPlaying)
+            {
+                audioSource.Play();
+            }
+            else
+            {
+                audioSource.Stop();
+            }
         }
         if (Input.GetKey(KeyCode.A))
         {
