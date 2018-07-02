752c78f65d34328193192a182bd4eea5050155ec
BLACKBOX\dakil <dakilangmanok@gmail.com>
2018-07-02 01:11:25 -0400

    Added the beginnings of moving part of the scene programattically.

--- Thrustmaster/Assets/Scripts/Oscilator.cs
+++ Thrustmaster/Assets/Scripts/Oscilator.cs
@@ -2,15 +2,25 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[DisallowMultipleComponent]
 public class Oscilator : MonoBehaviour {
+
+	[SerializeField] Vector3 movementVector;
+
+	[Range(0,1)]
+	[SerializeField]
+	float movementFactor;
+
+	Vector3 startingPos;
 
 	// Use this for initialization
 	void Start () {
-		
+	startingPos = transform.position;	
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		Vector3 offset = movementVector * movementFactor;
+		transform.position = startingPos + offset;
 	}
 }
