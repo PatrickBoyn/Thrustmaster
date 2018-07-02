ae9d4e9a90858613e222517bb7c53db53d94bfb3
BLACKBOX\dakil <dakilangmanok@gmail.com>
2018-07-02 02:18:31 -0400

    added a multiplication of Time.deltatime to the thrust method.

--- Thrustmaster/Assets/Scripts/Oscilator.cs
+++ Thrustmaster/Assets/Scripts/Oscilator.cs
@@ -5,8 +5,8 @@ using UnityEngine;
 [DisallowMultipleComponent]
 public class Oscilator : MonoBehaviour {
 
-	[SerializeField] Vector3 movementVector;
-
+	[SerializeField] Vector3 movementVector = new Vector3(10f, 10f, 10f);
+	[SerializeField] float period = 2f;
 	[Range(0,1)]
 	[SerializeField]
 	float movementFactor;
@@ -20,7 +20,14 @@ public class Oscilator : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+		
+		float cycles = Time.time /period;
+		const float tau = Mathf.PI * 2;
+		float rawSin = Mathf.Sin(cycles * tau);
+		print(rawSin);
+		movementFactor = rawSin/2f + 0.5f;
 		Vector3 offset = movementVector * movementFactor;
+
 		transform.position = startingPos + offset;
 	}
 }
--- Thrustmaster/Assets/Scripts/Rocket.cs
+++ Thrustmaster/Assets/Scripts/Rocket.cs
@@ -114,7 +114,7 @@ public class Rocket : MonoBehaviour {
 
     private void ApplyThrust()
     {
-        rigidbody.AddRelativeForce(Vector3.up * mainThrust);
+        rigidbody.AddRelativeForce(Vector3.up * mainThrust * Time.deltaTime);
         if (!audioSource.isPlaying)
         {
             audioSource.PlayOneShot(mainEngine);
